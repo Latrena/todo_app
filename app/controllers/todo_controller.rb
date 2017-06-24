88fc6e128b81ca28b161cfb92e7fec49ea3468a5
Latrena Weatherspoon <llweatherspo@cps.edu>
2017-06-24 19:06:21 +0000

    changes

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,14 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @pomodoro_estimate=4
-        @pomodoro_description="Make the curriculum"
+        todo_id = params[:id]
+        if todo_id == '1'
+             @pomodoro_estimate=4
+             @pomodoro_description="Make the curriculum"
+         end
+        if todo_id == '2'
+             @pomodoro_estimate=3
+             @pomodoro_description="Buy workshop supplies"
+        end
     end
 end

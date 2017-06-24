fb97e62ec61a0e0e47993eeb8b8a0310631f1fea
Latrena Weatherspoon <llweatherspo@cps.edu>
2017-06-24 19:20:16 +0000

    pomodoro changes

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -11,5 +11,25 @@ class TodoController < ApplicationController
              @pomodoro_estimate=3
              @pomodoro_description="Buy workshop supplies"
         end
+        if todo_id == '3'
+             @pomodoro_estimate=7
+             @pomodoro_description="Meet with the volunteer trainers"
+        end
+        if todo_id == '4'
+             @pomodoro_estimate=1
+             @pomodoro_description= "Order food for Saturday and Sunday"
+        end
+        if todo_id == '5'
+             @pomodoro_estimate=2
+             @pomodoro_description= "Check pre-work assignments"
+        end
+        if todo_id == '6'
+             @pomodoro_estimate=1
+             @pomodoro_description= " Send workshop location to all the students"
+        end
+        if todo_id == '7'
+             @pomodoro_estimate= 0
+             @pomodoro_description= "Have a great workshop"
+        end
     end
 end

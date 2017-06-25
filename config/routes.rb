336e1837b2b7e2b5f1e9e9b22cdb4a52700da5f4
Latrena Weatherspoon <llweatherspo@cps.edu>
2017-06-25 15:42:00 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'todo/index', to:'todo#index'
   get 'todo/show/:id' , to:'todo#show'
+  get 'todo/new', to:'todo#new'
+  get 'todo/create', to:'todo#create'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

dd0361479e1f35fada978650df51d0cd2492e157
Latrena Weatherspoon <llweatherspo@cps.edu>
2017-06-25 16:18:46 +0000

    edits

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,18 @@
 class TodoController < ApplicationController
+    # list all todos
     def index
     end
+    
+    # display form to edit existing todo
+    def edit
+        @todo=Todo.find_by_id(params[:id])
+    end
+    
+    # display form to create new todo
     def new
     end
+    
+    # save todo information to database
     def create
         t=Todo.new
         t.description=params[:description]
@@ -11,37 +21,8 @@ class TodoController < ApplicationController
         redirect_to "/todo/show/#{t.id}"
     end
     
-
+    # display specific todo
     def show
         @todo=Todo.find_by_id(params[:id])
-        # todo_id = params[:id]
-        # if todo_id == '1'
-        #      @pomodoro_estimate=4
-        #      @pomodoro_description="Make the curriculum"
-        #  end
-        # if todo_id == '2'
-        #      @pomodoro_estimate=3
-        #      @pomodoro_description="Buy workshop supplies"
-        # end
-        # if todo_id == '3'
-        #      @pomodoro_estimate=7
-        #      @pomodoro_description="Meet with the volunteer trainers"
-        # end
-        # if todo_id == '4'
-        #      @pomodoro_estimate=1
-        #      @pomodoro_description= "Order food for Saturday and Sunday"
-        # end
-        # if todo_id == '5'
-        #      @pomodoro_estimate=2
-        #      @pomodoro_description= "Check pre-work assignments"
-        # end
-        # if todo_id == '6'
-        #      @pomodoro_estimate=1
-        #      @pomodoro_description= " Send workshop location to all the students"
-        # end
-        # if todo_id == '7'
-        #      @pomodoro_estimate= 0
-        #      @pomodoro_description= "Have a great workshop"
-        # end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'todo/show/:id' , to:'todo#show'
   get 'todo/new', to:'todo#new'
   get 'todo/create', to:'todo#create'
+  get 'todo/edit/:id',to: 'todo#edit'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
